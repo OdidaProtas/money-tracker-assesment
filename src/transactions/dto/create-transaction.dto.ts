855f96025d70a5e0a6d4045876e37d34a32e7a0c
Dre <bryodiiidah@gmail.com>
2023-02-02 14:07:12 +0300

    add transaction DTO

--- src/transactions/dto/create-transaction.dto.ts
+++ src/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,17 @@
-export class CreateTransactionDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateTransactionDto {
+  @ApiProperty()
+  walletId: string;
+
+  @ApiProperty({ required: false, default: 'credit' })
+  type?: string;
+
+  @ApiProperty({
+    required: false,
+  })
+  description?: string;
+
+  @ApiProperty({ default: 0.0 })
+  amount?: number = 0.0;
+}
